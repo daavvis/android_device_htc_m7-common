170218dc5a9bc47a0bbe24d35928c581b485038a
daavvis <daavvisc@gmail.com>
2018-02-20 14:59:24 -0500

    m7:camera update
    from: Change-Id: Ifdaf82855e9ccc3b9d8e13f4124e3670b803109d

--- camera/Android.mk
+++ camera/Android.mk
@@ -22,18 +22,23 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/../include \
     system/media/camera/include
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
+LOCAL_STATIC_LIBRARIES := libbase libarect
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils
+    libhardware liblog libcamera_client libutils libcutils libdl \
+    android.hidl.token@1.0-utils \
+    android.hardware.graphics.bufferqueue@1.0
 
 LOCAL_HEADER_LIBRARIES := libnativebase_headers
 
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
+
 
 include $(BUILD_SHARED_LIBRARY)
